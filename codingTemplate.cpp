f529088852c1dcdd222e59597e7a7302a9c9ceef
@yush P@nwar <ayushpanwar691@gmail.com>
2022-03-06 11:20:14 +0530

    cp coding template for codechef , codeforces, leetcode

---
+++ codingTemplate.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define mod9 1e9+7
+#define mod7 1e7+7
+#define out(X) cout<<(X?"YES\n":"NO\n")
+using namespace std;
+bool isSpal(string s){for(int i=0,n=s.size();i<n/2;i++)if(s[i]!=s[n-i-1])return 0;return 1;}
+
+void solve(){
+	
+}
+signed main(){
+ios::sync_with_stdio(NULL);
+cout.tie(0);
+cin.tie(0);
+out(1);
+}
