ba4175963d91bad7f78f9b9d0b68351edf232bbe
Ayush Panwar <75212147+PanwarAyush@users.noreply.github.com>
2022-07-21 14:20:26 +0530

    Update codingTemplate.cpp

--- codingTemplate.cpp
+++ codingTemplate.cpp
@@ -1,78 +1,63 @@
 #include<bits/stdc++.h>
-#define ll long long
-#define ull unsigned long long
-#define mod9 1e9+7
-#define mod7 1e7+7
-#define pll pair<ll, ll>
-#define vll vector<ll>
-#define sll set<ll>
-#define db long double
-#define vpl vector<pair<ll, ll>>
-#define mll map<ll, ll>
+using namespace std;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
 #define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
 #define tc    \
     ll t;     \
     cin >> t; \
     while (t--)
-#define f(i, a, b) for (ll i = a; i < b; ++i)
-#define rev(i, a, b) for (ll i = a; i >= b; --i)
-#define all(x) x.begin(), x.end()
-#define sz(x) (ll)(x).size()
-#define print(x) cout << x << endl
-#define ct(x) cout << x << " "
-#define case (n) cout << "Case #" << n + 1 << ": "
-#define endl '\n'
-#define F first
-#define S second
-#define pi 3.1415926536
-#define gcd __gcd
-#define out(X) cout<<(X?"YES\n":"NO\n")
-using namespace std;
+#define out(X) cout<<(X?"YES\n":"NO\n")      
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
 bool isSpal(string s){for(int i=0,n=s.size();i<n/2;i++)if(s[i]!=s[n-i-1])return 0;return 1;}
- /*struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
-  };
-   struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-  };
-  */
-  class Trie {
-	public:
-     bool isEnd=0;
-     Trie *arr[26];
-  };
-  class Solution {
-      unordered_map<string,string>mp;
-  public:
-vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-          string s;
-          vector<double>ans;
 
-      }
-  };
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 void solve(){
+  
+}
+int main() {
+#ifndef ONLINE_JUDGE
   freopen("input.txt","r",stdin);
   freopen("output.txt","w",stdout);
-  Solution *sol=new Solution();
-  vector<vector<string>>&equations;
-  vector<double>values;
-  vector<vector<int>>queries;
-
-
+  freopen("error.txt", "w", stderr);
+#endif
+  tc
+  solve();
 }
-signed main(){
-ios::sync_with_stdio(NULL);
-cout.tie(0);
-cin.tie(0);
-solve();
-}
-/*g++ -g -Wall -Wno-unused-result -Werror=return-type -Werror=main -pipe -O2 -D_FORTIFY_SOURCE=2 -fno-delete-null-pointer-checks -std=c++14 -I/usr/include/jsoncpp Solution.cpp -o Solution -L/usr/include/jsoncpp -lm -ljsoncpp -lpthread -lcurl*/
